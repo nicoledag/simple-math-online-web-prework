188514369c812e520b6c36a4eef769b04db84349
Nicole D <nnardella@gmail.com>
2018-09-18 00:11:54 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,7 +1,9 @@
-def addition(num1, num2)
+def addition(num1=5, num2=4)
+  num1+num2
 end
 
-def subtraction(num1, num2)
+def subtraction(num1=10, num2=5)
+  num1-num2
 end
 
 def division(num1, num2)
